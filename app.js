b03c03267b13f37673da18f01f228354128bd8bf
dmytroKr <dmitryk@dev.tagsoft.co>
2022-03-25 14:45:18 +0200

    add proxy for websockets

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 const WebSocketServer = require('websocket').server;
 const httpProxy = require('http-proxy');
-const proxy = httpProxy.createProxyServer();
+const proxy = httpProxy.createProxyServer({
+    target: 'http://localhost:8080',
+    ws: true
+});
 const routes = require('./routes/root')
 const wsRoute = require('./websocket/root')
 
@@ -9,7 +12,7 @@ server = http.createServer(async function (req, res) {
     if(req.url === '/eth_getBlock') await routes.eth_getBlock(req, res, proxy)
     else if(req.url === '/eth_getLogs') await routes.eth_getLogs(req, res, proxy)
     else if(req.url === '/eth_getTxReceipt') await routes.eth_getTxReceipt(req, res, proxy)
-    else proxy.web(req, res, { target: 'http://localhost:8080' })
+    else proxy.web(req, res)
 });
 
 server.listen(9000, function (){
@@ -27,7 +30,7 @@ wsServer.on('request', async function(request) {
         if(request.resource === '/eth_getBlock') await wsRoute.eth_getBlock(message, request, request.socket, request.httpRequest.rawHeaders, proxy)
         else if(request.resource === '/eth_getLogs') await wsRoute.eth_getLogs(connection)
         else if(request.resource === '/eth_getTxReceipt') await wsRoute.eth_getTxReceipt(connection)
-        else proxy.ws(request, request.socket, request.httpRequest.rawHeaders, { target: 'http://localhost:8080' })
+        else proxy.ws(request, request.socket, request.httpRequest.rawHeaders)
     })
     console.log((new Date()) + ' Connection accepted.');
     connection.on('close', function(reasonCode, description) {
